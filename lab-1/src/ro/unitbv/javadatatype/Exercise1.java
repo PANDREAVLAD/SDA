503db6c6234df9c1a1b415b255626fcfbfdb745d
PANDREAVLAD <pandreavlad2@yahoo.com>
2020-03-04 16:56:11 +0200

    first exercise

--- lab-1/src/ro/unitbv/javadatatype/Exercise1.java
+++ lab-1/src/ro/unitbv/javadatatype/Exercise1.java
@@ -20,9 +20,28 @@ public class Exercise1 {
 		
 		int nr2= scanner.nextInt();
 		System.out.println("numarul 2 citit este: "+nr2);
-		int s=nr1+nr2;
-		System.out.println("suma este: "+s);
+		int suma=nr1+nr2;
+		System.out.println("suma este: "+suma);
 		System.out.println("diferenta este: " +(nr1-nr2));
+		System.out.println("produsul este: "+(nr1*nr2));
+		//nr1/nr3 afisat cu zecimale
+		double impartire =(double)nr1/nr2;
+		int impartireInt = nr1/nr2;
+		System.out.println("Impartirea este: "+impartire);
+		System.out.println("media este: "+((nr1+nr2)/2));
+		if(nr1>nr2)
+			System.out.println("DIstanta este: "+(nr1-nr2));
+		else
+			System.out.println("DIstanta este: "+(nr2-nr1));
+
+
+		if(nr1>nr2)
+			System.out.println("intregul maxm este: "+nr1+"(Nr 1)");
+		else
+		 if(nr1<nr2)
+			System.out.println("Intregul maxim este: "+nr2+"(Nr 2)");
+		 else
+			System.out.println("Nr sunt egale"+nr1);
 	}
 
 }
